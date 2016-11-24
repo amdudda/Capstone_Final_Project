32a7c40e7b9a452052337429862336be551ab6cd
amdudda <tk0654wm@minneapolis.edu>
2016-11-24 17:41:22 -0600

    Created index page and set up initial routes/views.

--- PatternGenerator/views.py
+++ PatternGenerator/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def index(request):
+    context = { 'patterns':'nothing to see here',}
+    return render(request,'PatternGenerator/index.html',context)
