e2ff6ca78e36207092128a507375702efaa41523
amdudda <tk0654wm@minneapolis.edu>
2016-11-24 18:06:56 -0600

    Practice passing static image info - this will eventually be changed to pass an array of info and process each of the SourceImage objects passed.

--- PatternGenerator/admin.py
+++ PatternGenerator/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import PatternImage, SourceImage
+
+my_models = [SourceImage,PatternImage]
+admin.site.register(my_models)
--- PatternGenerator/views.py
+++ PatternGenerator/views.py
@@ -3,5 +3,5 @@ from .models import *
 # Create your views here.
 
 def index(request):
-    context = { 'patterns':'nothing to see here',}
+    context = { 'patterns':'nothing to see here','mainimg':'2016-11-18-12-51_sunflowers-1719119_150.jpg'}
     return render(request,'PatternGenerator/index.html',context)
