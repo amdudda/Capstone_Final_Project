79fcfb86d53cf7236ed807868f787e5f0cee05db
amdudda <tk0654wm@minneapolis.edu>
2016-11-26 10:01:13 -0600

    Able to pass a single, pre-selected, image's pattern to viewpatterns page.

--- PatternGenerator/urls.py
+++ PatternGenerator/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns =[
 #    url(r'^search', views.search, name='search'),
+    url(r'^ViewPattern', views.viewpatterns, name='viewpatterns'),
     url(r'^$', views.index, name='index'),
 ]
--- PatternGenerator/views.py
+++ PatternGenerator/views.py
@@ -7,3 +7,14 @@ def index(request):
     # context = { 'patterns':'nothing to see here','mainimg':'2016-11-18-12-51_sunflowers-1719119_150.jpg'}
     context = { 'patterns':patterns }
     return render(request,'PatternGenerator/index.html',context)
+
+def viewpatterns(request):
+    # need to get patterns for requested image
+    # TODO for now we're not fussing with extracting selected image, just prove we can pass data to this view.
+    w = SourceImage.objects.get(id=1)
+    w_patterns = PatternImage.objects.filter(source_id=w)
+    context = {
+        'pattern_id': w.id,
+        'pattern_set': w_patterns
+        }
+    return render(request,'PatternGenerator/ViewPattern.html',context)
