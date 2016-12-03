fa660c2be510e6d9581ce7cd763e2bbce9a69eda
amdudda <tk0654wm@minneapolis.edu>
2016-12-03 11:49:15 -0600

    Use time.strftime() to generate timestamp.

--- PatternGenerator/ImageTools/ImageDownloader.py
+++ PatternGenerator/ImageTools/ImageDownloader.py
@@ -3,8 +3,8 @@ Downloads images based on a url passed to code.
 WARNING: assumes the url has been vetted by Django form validation.
 '''
 from PIL import Image
-from datetime import datetime
 from ImageValidator import isImage
+import time
 
 def FetchImage(url,directory="."):
     '''
@@ -19,19 +19,32 @@ def FetchImage(url,directory="."):
 
     if isAnImage:
         # let's establish the image filename and storage location
-        index = url.rfind('/') + 1
-        img_fname = url[index:]
-        img_fname = img_fname[-100:] # get the last 100 characters only, prepend it with a timestamp to ensure high likelihood of unique filenames
-        dt = datetime.now()
-        ts_arr = [dt.year , dt.month , dt.day , dt.hour , dt.minute , dt.second]
-        for el in range(len(ts_arr)):
-            ts_arr[el] = str(ts_arr[el])
-        ts = "_".join(ts_arr) + "_"
-        img_fname = ts + img_fname
+        img_fname = getFileName(url)
         print(img_fname)
         return True
     else:
         return False
+
+
+def getFileName(url):
+    '''
+    returns a file name t
+    :param url: Url of the source file
+    :return: string with a file name prepended by a timestamp
+    '''
+    index = url.rfind('/') + 1
+    img_fname = url[index:]
+    # get the last 100 characters only, prepend it with a timestamp to ensure high likelihood of unique filenames
+    img_fname = img_fname[-100:]
+    # dt = datetime.now()
+    # ts_arr = [dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second]
+    # for el in range(len(ts_arr)):
+    #     ts_arr[el] = str(ts_arr[el])
+    # ts = "_".join(ts_arr) + "_""
+    format = "%Y%m%d%H%M%S_"
+    ts = time.strftime(format)
+    img_fname = ts + img_fname
+    return img_fname
 
 
 if __name__ == "__main__":
