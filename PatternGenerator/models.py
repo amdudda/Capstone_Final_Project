f867f0723a669d3c5f7fc7b4ce6d94a23be3e6f0
amdudda <tk0654wm@minneapolis.edu>
2016-12-03 08:03:52 -0600

    update SourceImage model to store size in inches rather than pixels b/c calculating and passing this metatdata is a royal PITA.

--- PatternGenerator/models.py
+++ PatternGenerator/models.py
@@ -7,17 +7,17 @@ from django.db import models
 class SourceImage(models.Model):
     # this stores data bout the source image
     filename = models.CharField("file name",max_length=200)
-    width = models.IntegerField("image width in pixels",default=0)
-    height = models.IntegerField("image height in pixels",default=0)
+    width = models.DecimalField("image width in inches",decimal_places=2,max_digits=6)
+    height = models.DecimalField("image height in inches",decimal_places=2,max_digits=6)
     saved = models.DateTimeField("saved date",auto_now_add=True) # auto_now_add sets value to creation time
 
     def __str__(self):
-        return "Image filename: " + self.filename + ", last saved: " + str(self.saved)
+        return "Image filename: " + self.filename + ", saved: " + str(self.saved)
 
 class PatternImage(models.Model):
-    # TODO: what do I actually want to store about the bitmaps?
     # patterns can be reconstructed directly from bitmaps... color info, rgb info, all is stored there natively and
-    # would be redundant in the database other than to speed up retrieval.
+    # would be redundant in the database other than to speed up retrieval.  But some stuff I do want to store so I
+    # don't have to brute-force calculate them every time I load an image!
     filename = models.CharField("file name", max_length=200)
     created = models.DateTimeField("creation date", auto_now_add=True)  # auto_now_add sets value to creation time
     spi = models.IntegerField("stitches per inch",default=10)
