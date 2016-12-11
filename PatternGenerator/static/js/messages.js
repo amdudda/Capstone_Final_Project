7a1c15f9c0e99b27875c889f19a53e5f3fc63b40
amdudda <tk0654wm@minneapolis.edu>
2016-12-11 10:56:55 -0600

    Move JS to separate file and reuse for GenPatt template.

---
+++ PatternGenerator/static/js/messages.js
@@ -0,0 +1,9 @@
+/**
+ * Created by amdudda on 12/11/16.
+ * This contains code to manage messages on the templates.
+ */
+
+// Shows a "wait for image to process" message on GeneratePattern and UploadImage pages.
+function show_wait_msg(){
+    document.getElementById("please_wait").style.visibility = "visible";
+}
